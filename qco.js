ebe31c45872f2c0c0b7989d46f21ec27f088b481
arkuka <40722785+arkuka@users.noreply.github.com>
2025-03-19 20:56:45 +1100

    Update qco.js

--- qco.js
+++ qco.js
@@ -339,16 +339,12 @@ document.addEventListener("DOMContentLoaded", () => {
       // 读取第二个工作表（版本信息）
       const sheet2 = workbook.Sheets[workbook.SheetNames[1]];
       const versionData = XLSX.utils.sheet_to_json(sheet2, { header: 1 });
-      const versionInfo = versionData[1][0]; // 获取第二行第一列的值（A2）      
-      const currentVer = "ver:" + versionInfo
-      } catch (error) {
-        console.error("Failed to load or parse the Excel file:", error);
-      }
-
+      const versionInfo = versionData[1][0]; // 获取第二行第一列的值（A2）
+  
       // 显示版本号
       const versionInfoElement = document.getElementById("versionInfo");
       if (versionInfoElement && versionInfo) {
-        versionInfoElement.innerHTML = currentVer
+        versionInfoElement.innerHTML  = `ver:${versionInfo}`;
       }
   };
 
